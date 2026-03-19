1c90611c96f733357b12f22e32f551f8fa019a3e
Yasin Kip <ykip@student.unimelb.edu.au>
2026-03-19 15:41:20 +1100

    Included async code in tests

--- tests/test.py
+++ tests/test.py
@@ -2,10 +2,12 @@ from __future__ import annotations
 
 from pathlib import Path
 from typing import TYPE_CHECKING
+import asyncio
 
 from dotenv import load_dotenv
 
 from text2markdown.t2md import text_to_markdown as t2md
+from text2markdown.async_t2md import text_to_markdown_async as t2mda
 
 if TYPE_CHECKING:
     from os import PathLike
@@ -37,12 +39,10 @@ def no_api_key() -> bool:
     return err
 
 
-def run_default() -> bool:
+def run_default() -> None:
     """ Calls `text_to_markdown` on all tests in `/test_in`
     with default args and saves output in `/test_out`
     """
-    load_dotenv()
-
     tests = sorted([test for test in  TEST_DIR.iterdir() if test.is_file() and test.suffix == ".txt"])
     for test in tests:
         txt = t2md(get_text(test))
@@ -50,12 +50,21 @@ def run_default() -> bool:
         with open(TEST_OUT_DIR / f"{test.stem}.md", "w") as f:
             f.write(txt)
 
-def test():
+async def run_async() -> None: 
+    tests = sorted([test for test in  TEST_DIR.iterdir() if test.is_file() and test.suffix == ".txt"])
+    for test in tests:
+        # don't actually do anything, just check that there's no errors
+        resp = await t2mda(get_text(test))
+        assert(isinstance(resp, str))
+
+async def test():
     TEST_OUT_DIR.mkdir(exist_ok=True)
     TEST_DIR.mkdir(exist_ok=True)
     assert no_api_key()
-    run_default()
 
+    load_dotenv()
+    run_default()
+    await run_async()
 
 if __name__ == "__main__":
-    test()
+    asyncio.run(test())
